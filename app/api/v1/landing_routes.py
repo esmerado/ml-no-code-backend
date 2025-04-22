41306f8fcc0e153b78c7fa8427434ed400ac35f4
Javier Esmerado <esmerado.vela.javier@gmail.com>
2025-04-22 23:49:34 +0200

    Add all dependencies

--- app/api/v1/landing_routes.py
+++ app/api/v1/landing_routes.py
@@ -1,27 +1,34 @@
 import os
 
-from dotenv import load_dotenv
 from fastapi import APIRouter
 from fastapi import Request
 from supabase import create_client
 
 router = APIRouter()
-load_dotenv()
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
-print('SUPABASE_URL', SUPABASE_URL)
-supabase = create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
+
+def get_supabase_client():
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
+    return create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
 
 
 @router.post("/landing/new-email")
 async def add_user(request: Request):
+    print("📩 Recibida petición al endpoint /new-email")
     body = await request.json()
     email = body.get("email")
+    print("📩 email:", email)
 
-    # Guarda en tu tabla
+    supabase = get_supabase_client()
+
     res = supabase.table("emails").insert({
         "email": email,
     }).execute()
 
     return {"message": "Usuario guardado", "user": res.data}
+
+
+@router.get("/")
+def root():
+    return {"message": "API working"}
--- main.py
+++ main.py
@@ -3,15 +3,18 @@
 # Init
 # uvicorn main:app --reload
 
+import logging
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from openai import OpenAI
 
 from app.api.v1 import landing_routes
 
 app = FastAPI()
+print("📦 Railway ENV VARS:", dict(os.environ))
 
-client = OpenAI()
+logging.basicConfig(level=logging.DEBUG)
 
 app.add_middleware(
     CORSMiddleware,
@@ -20,7 +23,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-#
+
 # app.include_router(model_routes.router, prefix="/ml_backend/v1")
 # app.include_router(supabase_routes.router, prefix="/ml_backend/v1")
+
+
 app.include_router(landing_routes.router, prefix="/ml_backend/v1")
